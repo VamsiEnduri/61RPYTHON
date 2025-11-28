a46c16b4ec5787a45e8655ff24d0d9689b0ec01a
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2025-11-28 14:01:11 +0530

    pandas day intro

---
+++ pandas/intro.py
@@ -0,0 +1,29 @@
+import pandas as p
+# bentlydataset=p.read_excel("../../../../Downloads/bentley_car_sales.xlsx")
+# csvFile=p.read_csv("../../../../Downloads/BMW sales data (2010-2024) (1).csv")
+# jsonFile=p.read_json("./data.json")
+# # print(type(bentlydataset),"")
+# # print(csvFile,"bmw data")
+# print(jsonFile)
+
+# a=p.Series(["vamsi",27],["ravi",26],["sowmya",22])
+# print(a)
+
+# a=p.DataFrame([("vamsi",27,76000,"hyd"),("ravi",26,56900,"hyd"),("sowmya",22,0,"andra")],columns=["name","age","salary","loc"],index=["one","two","three"])
+# print(a)
+
+# a=p.DataFrame([{"name":"vamsi","age":27},{"name":"ravi","age":26}],index=[1,2])
+# print(a)
+data={"id":[1,2,3,4,5,6,7,8,9,10],"name":["a","b","c","d","e","f","g","h","i","j"]}
+a=p.DataFrame(data)
+print(a)
+# {
+#     "names":[]
+# }
+# id , name, age, loc, dept
+
+
+req-res life cycle fe-be client-server
+django antey yenti
+django yekkada use cheyyocchu (apps yem build cheyyocchu)
+django yenduku famous (django features)
